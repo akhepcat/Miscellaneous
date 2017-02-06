b5fa0adf3bc5df09255e55afc4cc659f133d4c2a
Leif Sawyer <ak.hepcat@gmail.com>
2017-02-06 10:02:23 -0900

    sorting ip addresses

---
+++ ipsort.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use Socket;
+
+my @in;
+my $DEBUG=0;
+
+while(<>) {
+   chop;
+   next if ($_ == "");
+   print "DBG3: in=($_)\n" if $DEBUG >= 3;
+
+   if (m/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*/) {
+     print "DBG2: ip_found($1)\n" if $DEBUG >= 2;
+     eval { my $x=inet_ntoa(inet_aton($1)) };
+     next if $@ ne "";
+     print "DBG1: ip_push($1)\n" if $DEBUG >= 1;
+     push @in, $1;
+   }
+}
+
+print "DBG5: unsorted IPs (@in)\n" if $DEBUG >= 5;
+
+my @sorted = map inet_ntoa($_), sort map inet_aton($_), @in;
+
+foreach $ip (@sorted) {
+  print "$ip\n";
+}
+
