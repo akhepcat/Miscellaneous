35da699631c92b0411d3e46e6004d78428a9ea09
Leif Sawyer <ak.hepcat@gmail.com>
2020-03-30 08:48:32 -0800

    allow for parsing/excluding localhost

--- listeners.sh
+++ listeners.sh
@@ -4,21 +4,35 @@
 #       lsof -i -n -P | grep LISTEN | sort -n -k +2 -t: | grep -vE '127.0.0.1|::1' | uniq -f 7
 # without multiple execs
 
+if [ -n "$1" -a "${1}" = "-l" ]
+then
+	FILTER="all"
+else
+	FILTER="local"
+fi
+
 GAWK=$(which gawk)
 AWK=$(which awk)
 
 USE_AWK=${GAWK:-$AWK}
 
-lsof -i -n -P +c15 | ${USE_AWK} '
+lsof -i -n -P +c15 | ${USE_AWK} -v filter=${FILTER} '
 # LSOF replaces spaces in the procname with \x20, which we CAN remove, else formatting
 #	$0 ~ /\\x20/ {
 #		gsub("\\\\x20"," ",$0)
 #	}
 
 
-# Look for global listeners (ignore localhost)
-	/LISTEN/ && !( /127.0.0.1/ || /::1/ ) {
+# Look for global listeners (ignore localhost if flagged)
+	/LISTEN/ {
+		if ( (filter ~ /local/) && (/127.0.0.1/ || /::1/) ) {
+		   next;
+		}
 		myline=$0;
+		if ( filter ~ /all/ ) {
+		  gsub("127.0.0.1","",myline);
+		  gsub("::1","",myline);
+		}
 		FS=":";
 		split($2,portArr," ");
 		port=portArr[1];
