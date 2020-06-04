b1d5af1a8586de79abad7b8aa47b7fab32d9ad8d
Leif Sawyer <leif@denali.net>
2020-06-04 15:48:38 -0800

    figure out a lower minimum for very low-memory devices (rPi-v1, say)

--- free-mem.sh
+++ free-mem.sh
@@ -3,15 +3,24 @@
 # Dumb cron-able script to drop caches every night and free up some memory.
 # Is it *really* needed?  probably not.  Until it is.
 # 
-# we used to use MemTotal, but really MemAvailable is probably "more right" more often
+# we use MemTotal to figure out a baseline minimum, and then check against MemAvailable
+# to see which minimum is probably "more right"
+
+kTotl=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 kInst=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
 kFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
 
+kMin=$(( ${kTotl:-0} / 4 ))
+if [ ${kMin:-0} -gt 1024 -a ${kMin:-0} -gt 65536 ]
+then
+        kMin=65536
+fi
+
 kLim=$(( kInst / 4 ))
 
-if [ ${kLim:-0} -lt 65536 ]
+if [ ${kLim:-0} -gt ${kMin} ]
 then
-        kLim=65536
+        kLim=${kMin}
 fi
 
 if [ ${kFree:-0} -lt ${kLim} ]
