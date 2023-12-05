0924ffa2eab3deac7c23b86f0fa6227ed650f7df
Leif Sawyer <leif@akhepcat.com>
2023-12-04 16:42:28 -0900

    Fixes for recent hashcat parsing, cleanup, and better detection of device types

--- oclhcbench2import.pl
+++ oclhcbench2import.pl
@@ -15,6 +15,7 @@ while (<>) {
 	chomp;
 
 	next if (m/^#/);
+	next if (m/skipped/);
 
 	if (m/^\*,999999,System Info,\d,(.*);(.*);(.*);(.*);(.*)/) {
 	      # *,999999,System Info,0,orange;GNU/Linux;5.10.110-rockchip-rk3588;aarch64;7873636kb
@@ -28,6 +29,18 @@ while (<>) {
 		# 1,999999,Device Info,0,pthread-cortex-a55; 2788/5641 MB (1024 MB allocatable); 8MCU
 		$dev=$1; $device{$dev}{"cpu"}=$2; $device{$dev}{"cache"}=$3; $device{$dev}{"cores"}=$4;
 
+		$_ = $device{$dev}{"cpu"};
+		if (m/pthread/i || m/cpu/i || m/processor/i || m/cortex/i) {
+			$DEV="CPU";
+			$device{$dev}{"cpu"} =~ s/pthread-//g;
+			$device{$dev}{"cpu"} =~ s/cortex-/ARM /g;
+			$device{$dev}{"cpu"} =~ s/ \w+-Core Processor//g;
+			
+		} else {
+			$DEV="GPU";
+		}
+		$device{$dev}{"dev"}=$DEV;
+
 	} elsif (m/^(\d+),(\d+),(.*),(\d+),(.*)/) {
 		# 1,7100,macOS v10.8+ PBKDF2-SHA512 (Iterations: 1023),4175,(195.46ms) @ accel:512 loops:255 thr:1 vec:2
 		$dev=$1; $mode=$2; $device{$dev}{$mode}{"desc"}=$3; $device{$dev}{$mode}{"results"}=$4; $device{$dev}{$mode}{"notes"}=$5;
@@ -40,23 +53,24 @@ for $d (keys %device) {
 	print "DBG: found device $d\n" if $DEBUG;
 
 	for $m (sort { $a <=> $b } keys %{$device{$dev}}) {
+		my $ores,$res;
 		next if (! looks_like_number($m) );
 		print "DBG: found mode $m\n" if $DEBUG;
-		$res=$device{$d}{$m}{"results"};
-		if ($res > 1000000000) {
-			$res=($res / 1000000000);
-			$unit="GHs";
-		} elsif ($res > 1000000) {
-			$res=($res / 1000000);
+		$ores=$device{$d}{$m}{"results"};
+		if ($ores > 1000000000) {
+			$res=($ores / 1000000000);
+			$unit="GH/s";
+		} elsif ($ores > 1000000) {
+			$res=($ores / 1000000);
 			$unit="MH/s";
-		} elsif ($res > 1000) {
-			$res=($res / 1000);
+		} elsif ($ores > 1000) {
+			$res=($ores / 1000);
 			$unit="kH/s";
 		} else {
 			$unit="H/s";
 		}
-		
-		printf("%d,%s,%s,%s,%s,,%.1f %s,%s\n",
-			$m, $device{$d}{$m}{"desc"}, $host, "CPU", $device{$d}{"cpu"}, $res, $unit, $device{$d}{$m}{"notes"});
+
+		printf("%d,%s,%s,%s,%s,%.0f,%.1f %s,%s\n",
+			$m, $device{$d}{$m}{"desc"}, $host, $device{$d}{"dev"}, $device{$d}{"cpu"}, $ores, $res, $unit, $device{$d}{$m}{"notes"});
 	}
 }
